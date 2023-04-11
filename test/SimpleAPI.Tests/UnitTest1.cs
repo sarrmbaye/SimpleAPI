35d42fb78dcb0f48415d2981e07c3530d7abd6d4
Mbaye SARR <mbysarr@yahoo.fr>
2023-04-11 01:37:46 -0400

    Initial Commit

---
+++ test/SimpleAPI.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.Extensions.Logging;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Tests;
+
+public class UnitTest1
+{
+    
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
